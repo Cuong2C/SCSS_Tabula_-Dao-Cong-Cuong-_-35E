210bfe68794bc54620e24297b55ea8c26288a9ff
Cuong2C <dcongcuong2012@gmail.com>
2022-10-03 23:54:15 +0700

    push code

---
+++ main.js
@@ -0,0 +1,9 @@
+window.on('scroll', function() {
+        if ($(window).scrollTop() >= 200) {
+            $('header').addClass('sticky');
+        }
+        else
+        {
+            $('header').removeClass('sticky');
+        }
+    });
